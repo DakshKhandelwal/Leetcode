62699048ade35b78266b7d5543e8a9b6f7376166
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-08-14 15:00:21 +0530

    Time: 42 ms (32.58%), Space: 23 MB (93.77%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -8,12 +8,15 @@ public:
         //     swap(s[i],s[t-1]);
         //     t--;
         // }
-        int start=0, end=n-1;
-        while(start<end)
-        {
-            swap(s[start],s[end]);
-            start++;
-            end--;
-        }
+        
+        
+        // int start=0, end=n-1;
+        // while(start<end)
+        // {
+        //     swap(s[start],s[end]);
+        //     start++;
+        //     end--;
+        // }
+        reverse(s.begin(),s.end());
     }
 };
