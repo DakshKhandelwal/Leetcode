6dc192aacf99428e1b489505e1b537f086aa6123
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-08-14 18:52:09 +0530

    Time: 29 ms (63.72%), Space: 17.8 MB (55.29%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> v (n);
+        sort(nums.begin(), nums.end()); //sort array
+        
+        int j=n-1;
+        for(int i=1; i<n; i+=2)  //filling odd places with max elements
+        {
+            v[i]= nums[j];
+            j--;
+        }
+        
+        for(int i=0; i<n; i+=2) //filling even places
+        {
+            v[i]=nums[j];
+            j--;
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            nums[i]=v[i];
+        }
+    }
+};
