75367bc221b04ef27f132a73e6b84cf50d40604a
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-08-14 18:56:02 +0530

    Time: 68 ms (6.13%), Space: 17.7 MB (55.29%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> v (n);
+//         sort(nums.begin(), nums.end()); //sort array
+        
+//         int j=n-1;
+//         for(int i=1; i<n; i+=2)  //filling odd places with max elements
+//         {
+//             v[i]= nums[j];
+//             j--;
+//         }
+        
+//         for(int i=0; i<n; i+=2) //filling even places
+//         {
+//             v[i]=nums[j];
+//             j--;
+//         }
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             nums[i]=v[i];
+//         }
         int n=nums.size();
-        vector<int> v (n);
-        sort(nums.begin(), nums.end()); //sort array
+        priority_queue<int> pq (nums.begin(),nums.end());  //coping arr and initializing pq
         
-        int j=n-1;
-        for(int i=1; i<n; i+=2)  //filling odd places with max elements
+        for(int i=1; i<n; i+=2)
         {
-            v[i]= nums[j];
-            j--;
+            nums[i] = pq.top();
+            pq.pop();
         }
-        
-        for(int i=0; i<n; i+=2) //filling even places
+        for(int i=0; i<n; i+=2)
         {
-            v[i]=nums[j];
-            j--;
-        }
-        
-        for(int i=0; i<n; i++)
-        {
-            nums[i]=v[i];
+            nums[i] = pq.top();
+            pq.pop();
         }
     }
 };
