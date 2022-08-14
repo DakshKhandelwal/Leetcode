9ab1a1e76c19be8add807f87a7bdf7289c1e7d98
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-08-14 14:26:17 +0530

    Time: 8 ms (75.05%), Space: 7.5 MB (96.25%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        int n=s.size();
-        vector<int>v;
-        for(int i=0; i<n; i++)
-        {
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
-               s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
-                v.push_back(i);   //storeindex of vowel in vector
-        }
+//         int n=s.size();
+//         vector<int>v;
+//         for(int i=0; i<n; i++)
+//         {
+//             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
+//                s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+//                 v.push_back(i);   //storeindex of vowel in vector
+//         }
         
-        for(int start=0, end=v.size()-1; start<end; start++,end--)
+//         for(int start=0, end=v.size()-1; start<end; start++,end--)
+//         {
+//             swap(s[v[start]], s[v[end]]); //swap char at that index
+//         }
+//         return s;   //O(n)+O(n)  + some space O(n)
+        
+        int n=s.size();
+        int i=0, j=n-1;
+        while(i<j)
         {
-            swap(s[v[start]], s[v[end]]);
+            bool flag1=false, flag2=false;
+            
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||                                        s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')                
+                flag1=true;
+            
+            if(s[j]=='a' ||s[j]=='e' ||s[j]=='i' ||s[j]=='o' ||s[j]=='u' ||
+               s[j]=='A' ||s[j]=='E' ||s[j]=='I' ||s[j]=='O' ||s[j]=='U')
+                flag2=true;
+            
+            if(flag1 && flag2)
+            {
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+            if(!flag1)
+                i++;
+            
+            if(!flag2)
+                j--;      
         }
         return s;
     }
