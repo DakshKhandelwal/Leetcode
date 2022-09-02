0efe14563daa3d8a54366f8c27c870effe999fcb
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-09-02 09:24:04 +0530

    Time: 4 ms (91.34%), Space: 9.5 MB (87.46%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -3,15 +3,12 @@ public:
     int superEggDrop(int k, int n) {
         int dp[n+1][k+1];
         memset(dp,0,sizeof(dp));
-        
         int m=0;
         while(dp[m][k]<n)
         {
             m++;
-            for(int i=1; i<=k; i++)
-            {
-                dp[m][i] = dp[m-1][i-1] + dp[m-1][i] +1;
-            }
+            for(int i=1; i<=k ;i++)
+                dp[m][i]= dp[m-1][i-1]+dp[m-1][i]+1;
         }
         return m;
     }
