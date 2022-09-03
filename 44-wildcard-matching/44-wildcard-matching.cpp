1c2a2aaa8cc07fa20943c0c7d25de3fc31f38021
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-09-03 22:52:29 +0530

    Time: 106 ms (43.85%), Space: 6.8 MB (92.21%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -45,55 +45,83 @@ class Solution {
      bool isMatch(string s, string p) {
          int n=s.length();
          int m=p.length();
-         vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+//          vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
          
-         for(int i=0; i<n+1; i++)
-         {
-             for(int j=0; j<m+1; j++)
-             {
-                 if(i==0 && j==0)
-                     dp[i][j] = true;
+//          for(int i=0; i<n+1; i++)
+//          {
+//              for(int j=0; j<m+1; j++)
+//              {
+//                  if(i==0 && j==0)
+//                      dp[i][j] = true;
                  
-                 if(j==0 && i>0)
-                     dp[i][j] =false;
+//                  if(j==0 && i>0)
+//                      dp[i][j] =false;
                  
-                 if(i==0 && j>0)
-                 {
-                     bool flag = true;
-                     for(int k=0; k<j; k++)
-                     {
-                         if(p[k]!='*')
-                         {
-                             flag = false;
-                             break;
-                         }
-                     }
-                     dp[i][j]= flag;
-                 }
-             }
-         }
+//                  if(i==0 && j>0)
+//                  {
+//                      bool flag = true;
+//                      for(int k=0; k<j; k++)
+//                      {
+//                          if(p[k]!='*')
+//                          {
+//                              flag = false;
+//                              break;
+//                          }
+//                      }
+//                      dp[i][j]= flag;
+//                  }
+//              }
+//          }
          
+//          for(int i=1; i<n+1; i++)
+//          {
+//              for(int j=1; j<m+1; j++)
+//              {
+//                  if(s[i-1]==p[j-1] || p[j-1]=='?')
+//                      dp[i][j] = dp[i-1][j-1];
+                 
+//                  else if(p[j-1]=='*')
+//                      dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                 
+//                  else 
+//                      dp[i][j] = false;
+//              }
+//          }
+//          return dp[n][m];
+         
+         vector<bool> dp(m+1, false);
+         dp[0]=true;
+        vector<bool> temp(m+1,false);
+         for(int j=1;j<=m;j++){
+            int flag=0;
+            for(int x=0;x<j;x++){
+                if(p[x]!='*')flag++;
+            }
+            if(flag==0)dp[j]=1;
+        }
          for(int i=1; i<n+1; i++)
          {
+            /* vector<bool>temp(m+1, false);
+             bool flag=true;
+             for(int k=1;k<=i;k++)
+             {
+                 if(p[k-1]!='*')
+                 {
+                     flag=false;
+                     break;
+                 }
+             }
+             temp[0]=flag;*/
              for(int j=1; j<m+1; j++)
              {
                  if(s[i-1]==p[j-1] || p[j-1]=='?')
-                     dp[i][j] = dp[i-1][j-1];
-                 
+                     temp[j] = dp[j-1];
                  else if(p[j-1]=='*')
-                     dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                 
-                 else 
-                     dp[i][j] = false;
+                     temp[j] = dp[j] || temp[j-1];
+                 else temp[j]=false;
              }
+             dp=temp;
          }
-         
-         // for(int i=0; i<n+1; i++){
-         //     for(int j=0; j<m+1; j++)
-         //         cout<<dp[i][j]<<" ";
-         //     cout<<endl;
-         // }
-         
-         return dp[n][m];
+         return dp[m];
      }
 };
