c9258fed9bce69a6ee18d6e3e27dc31c27ab3ebf
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2022-09-12 22:32:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.75%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-//         int N = m+n-2;
-//         int r = m-1;
-//         double ans =1;
-//         for(int i=1; i<=r; i++) // N C r
-//             ans = ans * (N-r+i)/i;
+        int N = m+n-2;
+        int r = m-1;
+        double ans =1;
+        for(int i=1; i<=r; i++) // N C r
+            ans = ans * (N-r+i)/i;
         
-//         return (int)ans;
+        return (int)ans;
         
-        int dp[m+1][n+1];
-        for(int i=1; i<m+1; i++)
-        {
-            for(int j=1; j<n+1; j++)
-            {
-                if(i==1 || j==1)
-                    dp[i][j] = 1;
-            }
-        }
+//         int dp[m+1][n+1];
+//         for(int i=1; i<m+1; i++)
+//         {
+//             for(int j=1; j<n+1; j++)
+//             {
+//                 if(i==1 || j==1)
+//                     dp[i][j] = 1;
+//             }
+//         }
         
-        for(int i=2; i<m+1; i++)
-        {
-            for(int j=2; j<n+1; j++)
-            {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m][n];
+//         for(int i=2; i<m+1; i++)
+//         {
+//             for(int j=2; j<n+1; j++)
+//             {
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//             }
+//         }
+//         return dp[m][n];
     }
 };
